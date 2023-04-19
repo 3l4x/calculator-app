d1a5806eca36ebcdaa17baa1a404aabb78f9f339
3l4x <c0ltwinters@protonmail.com>
2023-04-19 14:22:32 +0200

    Set up basic project structure
    -Add routing with different layouts
    -Add a calculator component

---
+++ frontend/src/components/layout/AuthLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+//not used atm
+const AuthLayout = () => {
+  return (
+    <main>
+        <Outlet/>
+    </main>
+  )
+}
+
+export default AuthLayout
