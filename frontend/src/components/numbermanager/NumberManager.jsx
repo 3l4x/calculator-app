9b747382f483e705f515563efe5a21cb520800ca
3l4x <c0ltwinters@protonmail.com>
2023-04-20 11:03:02 +0200

    Add query + mutation to NumberManager and more
    -Add calculatorApi functionality to NumberManager
    -Add error/success msg that disappears after 2 seconds
    -Add basic error handling for response

--- frontend/src/components/numbermanager/NumberManager.jsx
+++ frontend/src/components/numbermanager/NumberManager.jsx
@@ -1,19 +1,109 @@
-import { Segment, Button,Header } from "semantic-ui-react"
+import { Segment, Button, Header, Form, Label, Divider } from "semantic-ui-react"
+import { selectUser } from "../../redux/auth/authSlice"
+import { useSelector } from "react-redux"
+import { useGetNumberQuery, usePostNumberMutation } from "../../redux/calculator/calculatorApiSlice"
+import { useCallback, useContext, useState } from "react"
+import { CalculatorContext } from "../../providers/CalculatorContext"
 const NumberManager = () => {
+    const clearMessages = useCallback(() => {
+        //clear messages after 2 seconds
+        setTimeout(() => {
+            setSuccessMsg('');
+            setErrorMsg('');
+        }, 2000)
+    }, [])
+
+    const [errorMsg, setErrorMsg] = useState('');
+    const [successMsg, setSuccessMsg] = useState('');
+    const { value, input, setInput } = useContext(CalculatorContext);
+    const user = useSelector(selectUser);
+
+    //query
+    const { data: number, isFetching, error: getError } = useGetNumberQuery(null, {
+        skip: !user
+    });
+
+    //mutation
+    const [postNumber, { isLoading }] = usePostNumberMutation();
+
     return (
         <>
-            <Header as='h2' style={{marginBottom: 20}} color='teal' textAlign='center'>
+            <Header as='h2' style={{ marginBottom: 20 }} color='teal' textAlign='center'>
                 Number manager
             </Header>
-            <Segment style={{display: 'flex', flexDirection: 'column', gap: 20}} stacked>
-                <Button color='teal' fluid size='large'>
-                    Save Number
-                </Button>
-                <Button color='teal' fluid size='large'>
-                    Get saved number
-                </Button>
-            </Segment>
+            {
+                user ? (
+                    <Segment style={{ display: 'flex', flexDirection: 'column', gap: 5 }} stacked>
+                        <Label size="large">User actions</Label>
+                        <Button color='teal' size='small'
+                            onClick={async () => {
+                                if (!value) {
+                                    setErrorMsg('No number to save. Please calculate something first.');
+                                    clearMessages();
+                                    return;
+                                }
+                                await postNumber(value)
+                                    .unwrap()
+                                    .then((response) => {
+                                        setSuccessMsg('Number saved successfully!');
+                                    }).catch((err) => {
+                                        if (!err.status) {
+                                            setErrorMsg('Internal server error');
+                                        }
+                                        else if (err.status === 404) {
+                                            setErrorMsg('User not found');
+                                        }
+                                        else if (err.status === 401) {
+                                            setErrorMsg('Invalid credentials');
+                                        }
+                                        else if (err.status === 400) {
+                                            setErrorMsg('Expression missing.');
+                                        }
+                                        else if (err.status === 422) {
+                                            setErrorMsg('Invalid expression provided.');
+                                        }
+                                        else {
+                                            /* console.log(err.data); */
+                                            setErrorMsg('Unknown error');
+                                        }
+                                    }).finally(() => {
+                                        clearMessages();
+                                    })
+                            }}
+                        >
+                            Save result to server
+                        </Button>
+                        <Button color='teal' size='small'
+                            style={{ marginBottom: 20 }}
+                            onClick={() => {
+                                console.log(number);
+                                if (number) setInput(number);
+                            }}
+                        >
+                            Load saved to calculator
+                        </Button>
+                        {errorMsg && <Label color='red' style={{ opacity: 0.5 }}>
+                            {errorMsg}
+                        </Label>}
+                        {successMsg && <Label color='green' style={{ opacity: 0.5 }}>
+                            {successMsg}
+                        </Label>}
 
+                        <Divider />
+                        <Label size="large">Currently saved number</Label>
+                        <Form.Input
+                            fluid
+                            error={(getError) ? true : false}
+                            disabled={true}
+                            value={(getError ?? (number ?? 'No number saved'))}
+                        />
+                    </Segment>
+                ) : (
+                    <Segment style={{ display: 'flex', flexDirection: 'column', gap: 20 }} stacked>
+                        You must be logged in to use this feature!
+                    </Segment>
+                )
+            }
         </>
     )
 }
