8876426aaf6bf716b5840ab68b6f364dad57be8e
3l4x <c0ltwinters@protonmail.com>
2023-04-20 10:58:34 +0200

    Add login/logout to header

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,31 +1,81 @@
-import {Menu, Container, Dropdown, Image}  from 'semantic-ui-react'
+import { Menu, Container, Dropdown, Image } from 'semantic-ui-react'
 import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { selectUser } from '../../redux/auth/authSlice';
+import { useLogoutMutation } from '../../redux/auth/authApiSlice';
+import { useDispatch } from 'react-redux';
+import { deleteCredentials } from '../../redux/auth/authSlice';
 const Header = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  const [logout, { isLoading }] = useLogoutMutation();
+  const [logoutError, setLogoutError] = useState('');
   const navigate = useNavigate();
   return (
     <Menu fixed='top' inverted>
-    <Container>
-      <Menu.Item
-        as='div'
-        header
-        onClick={() => navigate('/')}
-      >
-        Home
-      </Menu.Item>
-      <Menu.Item
-        as='div'
-        onClick={() => navigate('/login')}
-      >
-        Login
-      </Menu.Item>
-      <Menu.Item
-        as='div'
-        onClick={() => navigate('/register')}
-      >
-        Register
-      </Menu.Item>
-    </Container>
-  </Menu>
+      <Container>
+        <Menu.Item
+          as='div'
+          header
+          onClick={() => navigate('/')}
+        >
+          Home
+        </Menu.Item>
+
+        {
+          !user ? (
+            <>
+              <Menu.Item
+                as='div'
+                onClick={() => navigate('/login')}
+              >
+                Login
+              </Menu.Item>
+              <Menu.Item
+                as='div'
+                onClick={() => navigate('/register')}
+              >
+                Register
+              </Menu.Item>
+            </>
+
+          ) : (
+            <Menu.Item
+              as='div'
+              onClick={async () => {
+                await logout()
+                  .unwrap()
+                  .then(() => {
+                    dispatch(deleteCredentials());
+                  })
+                  .catch((err) => {
+                    if (!err.originalStatus) {
+                      setLogoutError('Internal server error');
+                    }
+                    else if (err.originalStatus === 401) {
+                      setLogoutError('Invalid logout request');
+                    }
+                    else {
+                      setLogoutError('Unknown error');
+                    }
+                    console.log(logoutError);
+                  })
+                  .finally(() => {
+                    setTimeout(() => {
+                      setLogoutError('')
+                      navigate('/');
+                      window.location.reload(); //refreshing page,emptying cache
+                    }, 1500)
+                  })
+              }}
+            >
+              Logout
+            </Menu.Item>
+          )
+        }
+      </Container>
+    </Menu>
   )
 }
 
